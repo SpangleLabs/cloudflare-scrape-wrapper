96208e1fd5d08af9ee7c198dee8dfdd635ec7052
joshcoales <joshua@coales.co.uk>
2019-09-13 07:58:10 +0100

    Creating a basic GET/POST python wrapper around this cloudscrape library

---
+++ run.py
@@ -0,0 +1,38 @@
+import json
+import cfscrape
+from flask import Flask, request
+
+app = Flask(__name__)
+scraper = cfscrape.create_scraper()
+
+with open("config.json", "r") as f:
+    config = json.load(f)
+
+if not config['web_domain']:
+    print("Please set a web_domain in config.json")
+    quit()
+
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all_get(path):
+    full_path = "{}/{}".format(config['web_domain'], path)
+    resp = scraper.get(full_path)
+    return resp.content, resp.status_code
+
+
+@app.route('/', defaults={'path': ''}, methods=['POST'])
+@app.route('/<path:path>', methods=['POST'])
+def catch_all(path):
+    full_path = "{}/{}".format(config['web_domain'], path)
+    if request.json:
+        data = request.values
+        resp = scraper.post(full_path, json=data)
+        return resp.content, resp.status_code
+    data = request.data
+    resp = scraper.post(full_path, data=data)
+    return resp.content, resp.status_code
+
+
+if __name__ == '__main__':
+    app.run(port=config['port'])
